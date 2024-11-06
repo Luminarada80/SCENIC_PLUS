64896c5fc7605e80c40ca8d743a21fa7a2ad0968
emoeller <luminarada80@gmail.com>
2024-11-06 16:17:05 -0500

    Added time and memory requirement tracking to run_scenic_plus.sh

--- run_scenic_plus.sh
+++ run_scenic_plus.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes=1
 #SBATCH -c 16
 #SBATCH --mem-per-cpu=16G
-#SBATCH -o scenic_plus.log
-#SBATCH -e scenic_plus.err
+#SBATCH -o LOGS/scenic_plus.log
+#SBATCH -e LOGS/scenic_plus.err
 
 conda activate scenicplus
 cd /gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS
@@ -15,14 +15,23 @@ echo $(which python)
 # Add the local pycisTopic to the python path so it's recognized as a module
 export PYTHONPATH=$PYTHONPATH:/gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/pycisTopic/src
 
-# echo Running step00_RNA_preprocessing.py
-# python3 /gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/step00_RNA_preprocessing.py
+LOG_DIR="/gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/LOGS"
 
-echo Running step01_preprocessing_pseudobulk_profiles.py
-python3 /gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/step01_ATAC_preprocessing_pseudobulk_profiles.py
+# Function to run each Python step with timing and memory tracking
+run_step() {
+  step_name=$1
+  script_path=$2
+  shift 2  # Remove the first two arguments
+  echo "Running $step_name..."
+  
+  # Use /usr/bin/time with verbose output to capture memory and timing
+  /usr/bin/time -v python3 "$script_path" "$@" 2>> "${LOG_DIR}/${step_name}_time_mem.log"
+}
 
-echo Running step02_ATAC_inferring_consensus_peaks.py
-python3 /gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/step02_ATAC_inferring_consensus_peaks.py
+run_step "step00_RNA_preprocessing.py" "/gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/step00_RNA_preprocessing.py"
 
-echo Running step03_ATAC_qc.py
-python3 /gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/step03_ATAC_qc.py
+run_step "step01_preprocessing_pseudobulk_profiles.py" "/gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/step01_ATAC_preprocessing_pseudobulk_profiles.py"
+
+run_step "step02_ATAC_inferring_consensus_peaks.py" "/gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/step02_ATAC_inferring_consensus_peaks.py"
+
+run_step "step03_ATAC_qc.py" "/gpfs/Labs/Uzun/SCRIPTS/PROJECTS/2024.GRN_BENCHMARKING.MOELLER/SCENIC_PLUS/step03_ATAC_qc.py"
